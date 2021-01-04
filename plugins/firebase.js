325fec2cd6d5186582967fdc0dc0f1d54cac2deb
elanurdemir <58703987+elanurdemir@users.noreply.github.com>
2021-01-04 20:32:49 +0300

    nida yanlışlıkla sildi.

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -5,12 +5,13 @@ import 'firebase/auth'
 require('firebase/auth')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBboWg3jcLnKIeo5Q8BsVpCEGScKAdvkmA",
-    authDomain: "manuka-nuxt-bf3e6.firebaseapp.com",
-    projectId: "manuka-nuxt-bf3e6",
-    storageBucket: "manuka-nuxt-bf3e6.appspot.com",
-    messagingSenderId: "18753036510",
-    appId: "1:18753036510:web:8c74a77b9156dee699224f"
+    apiKey: "AIzaSyBDl0DImA2BlNJndznBci_rmIzYa8VhEEA",
+    authDomain: "manuka-nuxt.firebaseapp.com",
+    databaseURL: "https://manuka-nuxt-default-rtdb.firebaseio.com",
+    projectId: "manuka-nuxt",
+    storageBucket: "manuka-nuxt.appspot.com",
+    messagingSenderId: "848001636087",
+    appId: "1:848001636087:web:234e4fe76dbdb91876e5f2"
 }
 
 if (firebase.apps.length == 0) {
